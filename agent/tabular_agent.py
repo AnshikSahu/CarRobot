301095289923698adb8265637cf827c1d589ea05
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2024-03-30 06:06:52 +0530

    Add files via upload

---
+++ agent/tabular_agent.py
@@ -0,0 +1,37 @@
+import gymnasium as gym
+import car_gym
+from utils import SMALL_ENV, LARGE_ENV
+from utils import plot_rewards
+
+
+#Example USAGE
+# '''
+# env can be SMALL_ENV or LARGE_ENV
+
+# reset the environment: 
+#         observation, info = env.reset()
+# Next state on taking an action: 
+#         observation, reward, terminated, truncated, info = env.step(action)
+# Render the current_state:
+#     rgb_image = env.render
+
+# For more information about the gym API please visit https://gymnasium.farama.org/api/env/
+# New to OpenAI gym ?  Get started here https://gymnasium.farama.org/content/basic_usage/
+# '''
+
+## Keep your Tabular Q-Learning implementatio here
+
+#TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
